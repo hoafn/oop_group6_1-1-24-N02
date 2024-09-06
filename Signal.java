0eee8e558369cba2436f625b74472ad8181191ca
hoafn <121150870+hoafn@users.noreply.github.com>
2024-09-06 15:37:15 +0700

    Add files via upload

--- Signal.java
+++ Signal.java
@@ -1,7 +1,10 @@
-package Kiemtragiuaki.Cau2;
+package Kiemtragiuaki.Cau1;
 
+// Yêu cầu 1
 public interface Signal {
+    // Phương thức để gửi tín hiệu với một thông điệp
     void sendSignal(String message);
+
+    // Phương thức để nhận tín hiệu, trả về true nếu nhận thành công
     boolean receiveSignal();
 }
-
